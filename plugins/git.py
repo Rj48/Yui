f550f9a006b1d4a258e9f8c613f3792578275418
Rouji <rj@r0.at>
2017-03-12 19:20:14 +0100

    add git_pull cmd

---
+++ plugins/git.py
@@ -0,0 +1,20 @@
+import subprocess
+
+@yui.admin
+@yui.threaded
+@yui.command('git_pull')
+def git_pull(argv):
+    """Does a git pull on the working dir of the bot. Usage: git_pull [repo]"""
+
+    args = ['git', 'pull']
+    if len(argv) > 1:
+        args.append(argv[1])
+
+    proc = subprocess.Popen(args)
+    code = proc.wait()
+    if code == 0:
+        return "git pull's done!"
+    else:
+        return "git pull failed :("
+
+
