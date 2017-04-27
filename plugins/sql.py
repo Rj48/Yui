de69132c420f459c8de9bc63737d051dd7d95ab4
Rouji <rj@r0.at>
2017-04-27 02:00:02 +0200

    add sql plugin

---
+++ plugins/sql.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+
+@yui.admin
+@yui.command('sql')
+def sql(msg, user):
+    split = msg.split(' ', 1)
+    if len(split) < 2:
+        return
+
+    try:
+        c = yui.db.cursor()
+        c.execute(split[1])
+        text = '%s rows affected' % c.rowcount
+        rows = c.fetchall()
+        yui.db.commit()
+        if len(rows) > 0:
+            text = ''
+            for r in rows:
+                text += str(r)
+        return text
+    except Exception as e:
+        return str(e)
