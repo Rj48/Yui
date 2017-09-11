169ea131f0bee6cfa6044da4b7169390c83fe4d0
狼耳 <Rouji@users.noreply.github.com>
2017-09-11 08:42:16 +0200

    update hook searching in help plugin

--- plugins/help.py
+++ plugins/help.py
@@ -8,7 +8,7 @@ def help(argv):
     """Returns doc for commands. Usage: help <command>"""
     if len(argv) < 2:
         return 'Usage: help <command>'
-    hook = yui.hook_by_cmd(argv[1])
+    hook = yui.find_hook(lambda h: h.cmd == argv[1])
     if hook:
         doc = inspect.getdoc(hook.func)
         if doc:
