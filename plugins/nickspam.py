016bec706019a3d573d00929974e25b5d6aab38a
Rouji <rj@r0.at>
2018-01-01 18:17:12 +0100

    renamings

--- plugins/nickspam.py
+++ plugins/nickspam.py
@@ -3,25 +3,25 @@
 import re
 from collections import deque
 
-MAX_NICKS = yui.config_val('nickspam', 'maxNicks', default=10)
+MAX_HIGHLIGHTS = yui.config_val('nickspam', 'maxHighlights', default=10)
 MSG_COUNT = yui.config_val('nickspam', 'msgCount', default=4)
-USERS = {}
+HIGHLIGHTS = {}
 
 
 @yui.event('msgRecv')
-def antispam(msg, user, channel):
+def nickspam(msg, user, channel):
     # ignore query/pm
     if channel == user.nick:
         return
 
-    nicks_in_chan = yui.nicks_in_channel(channel)
-    num_highlight = sum([1 if nick in re.split('[.,;:!?\'" ]+', msg) else 0 for nick in nicks_in_chan])
+    chan_nicks = yui.nicks_in_channel(channel)
+    num_highlight = sum([1 if nick in re.split('[.,;:!?\'" ]+', msg) else 0 for nick in chan_nicks])
 
     key = channel + ' ' + user.nick
-    if key not in USERS.keys():
-        USERS[key] = deque(maxlen=MSG_COUNT)
-    USERS[key].append(num_highlight)
+    if key not in HIGHLIGHTS.keys():
+        HIGHLIGHTS[key] = deque(maxlen=MSG_COUNT)
+    HIGHLIGHTS[key].append(num_highlight)
 
-    if sum(USERS[key]) > MAX_NICKS:
+    if sum(HIGHLIGHTS[key]) > MAX_HIGHLIGHTS:
         yui.kick(channel, user.nick, 'spam')
-        return '%s spammed %d nicks in their last %d messages' % (user.nick, sum(USERS[key]), len(USERS.pop(key)))
+        return '%s spammed %d nicks in their last %d messages' % (user.nick, sum(HIGHLIGHTS[key]), len(HIGHLIGHTS.pop(key)))
