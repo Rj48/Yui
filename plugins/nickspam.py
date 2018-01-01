7f2f137f1f5d9bb6b4206da00f175ef18790c17a
Rouji <rj@r0.at>
2018-01-01 18:04:28 +0100

    kick msg, etc.

--- plugins/nickspam.py
+++ plugins/nickspam.py
@@ -10,14 +10,18 @@ USERS = {}
 
 @yui.event('msgRecv')
 def antispam(msg, user, channel):
+    # ignore query/pm
     if channel == user.nick:
         return
-    nicks = yui.nicks_in_channel(channel)
-    n = sum([1 if nick in re.split('[.,;:!?\'" ]+', msg) else 0 for nick in nicks])
+
+    nicks_in_chan = yui.nicks_in_channel(channel)
+    num_highlight = sum([1 if nick in re.split('[.,;:!?\'" ]+', msg) else 0 for nick in nicks_in_chan])
+
     key = channel + ' ' + user.nick
     if key not in USERS.keys():
         USERS[key] = deque(maxlen=MSG_COUNT)
-    USERS[key].append(n)
+    USERS[key].append(num_highlight)
+
     if sum(USERS[key]) > MAX_NICKS:
-        yui.kick(channel, user.nick)
-        return '%s spammed %d nicks in his last %d messages' % (user.nick, sum(USERS[key]), len(USERS[key]))
+        yui.kick(channel, user.nick, 'spam')
+        return '%s spammed %d nicks in their last %d messages' % (user.nick, sum(USERS[key]), len(USERS.pop(key)))
